031a14fe4eb09a02cf11742a289909e8161e99aa
NathanMaced <nathanmacedo02@gmail.com>
2022-10-27 21:18:37 -0300

    Criando o menu

--- app/src/main/java/br/com/etecia/foodfast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfast/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.etecia.foodfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+//Inserindo o menu na barra de aplicativo
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        return super.onCreateOptionsMenu(menu);
     }
 }
