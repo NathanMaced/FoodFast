ca18b09d3ccd7b23164d6a177d5ca135b96d6ee3
NathanMaced <nathanmacedo02@gmail.com>
2022-11-03 21:07:31 -0300

    Edição do menu superior

--- app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
+++ app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MenuAppBarTop_Activity extends AppCompatActivity {
+    MaterialToolbar idToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_app_bar_top_layout);
+        //apresentnado a toolbar xml para o java
+        idToolbar = findViewById(R.id.idToolBar);
     }
 }
