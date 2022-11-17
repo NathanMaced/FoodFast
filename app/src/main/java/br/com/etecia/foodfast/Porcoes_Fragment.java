25072b0cefbf88786eb18574fc8e32e0bdb3e496
NathanMaced <nathanmacedo02@gmail.com>
2022-11-17 19:56:43 -0300

    Uso do Fragment

--- app/src/main/java/br/com/etecia/foodfast/Porcoes_Fragment.java
+++ app/src/main/java/br/com/etecia/foodfast/Porcoes_Fragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Porcoes_Fragment extends Fragment {
+    EditText edtPorcoes;
+    Button btnEscolha;
 
 
     @Override
@@ -18,8 +22,8 @@ public class Porcoes_Fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_porcoes, container, false);
 
-
-
+        edtPorcoes = view.findViewById(R.id.edtPorcoes);
+        btnEscolha = view.findViewById(R.id.btnEscolha);
 
         return view;
     }
